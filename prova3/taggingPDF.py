c0d11ea9619e3b5ede00b20ff1cef5ad1de9a79b
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2023-06-14 14:00:42 +0000

    modifiche nomi pdf

--- prova3/taggingPDF.py
+++ prova3/taggingPDF.py
@@ -6,59 +6,61 @@ from pdfminer.high_level import extract_text
 taxonomy = [
     # Programming Languages
     "Python", "JavaScript", "Java", "C++", "C#", "Ruby", "Go", "Swift", "Rust", "PHP",
-    
+
     # Technology Companies
     "Tesla", "Apple", "Microsoft", "Google", "Amazon", "Facebook", "IBM", "Intel", "Nvidia", "Samsung",
-    
+
     # News and Media
-    "Breaking news", "Hea.dlines", "Journalism", "Broadcast", "Press release", "Media coverage", 
+    "Breaking news", "Hea.dlines", "Journalism", "Broadcast", "Press release", "Media coverage",
     "Editorial", "Op-ed", "Investigative reporting", "Public relations",
-    
+
     # Scientific Fields
-    "Physics", "Chemistry", "Biology", "Astronomy", "Geology", "Mathematics", "Psychology", 
+    "Physics", "Chemistry", "Biology", "Astronomy", "Geology", "Mathematics", "Psychology",
     "Sociology", "Anthropology", "Economics",
-    
+
     # Arts and Entertainment
-    "Film", "Music", "Literature", "Theater", "Visual arts", "Dance", "Television", "Gaming", 
+    "Film", "Music", "Literature", "Theater", "Visual arts", "Dance", "Television", "Gaming",
     "Photography", "Fashion",
-    
+
     # Sports
-    "Soccer", "Basketball", "Tennis", "Golf", "Baseball", "Athletics", "Swimming", "Cricket", 
+    "Soccer", "Basketball", "Tennis", "Golf", "Baseball", "Athletics", "Swimming", "Cricket",
     "Rugby", "Volleyball",
-    
+
     # Global Issues
-    "Climate change", "Human rights", "Poverty", "Education", "Healthcare", "Sustainability", 
+    "Climate change", "Human rights", "Poverty", "Education", "Healthcare", "Sustainability",
     "Gender equality", "Social justice", "Conflict resolution", "Immigration",
-    
+
     # Business and Finance
-    "Entrepreneurship", "Startups", "Investments", "Stock market", "Financial planning", "Banking", 
+    "Entrepreneurship", "Startups", "Investments", "Stock market", "Financial planning", "Banking",
     "Insurance", "Marketing", "E-commerce", "Supply chain",
-    
+
     # Travel and Tourism
-    "Destinations", "Hotels", "Airlines", "Vacation packages", "Sightseeing", "Adventure travel", 
+    "Destinations", "Hotels", "Airlines", "Vacation packages", "Sightseeing", "Adventure travel",
     "Sustainable tourism", "Travel insurance", "Cultural experiences", "Backpacking",
-    
+
     # Health and Wellness
-    "Nutrition", "Exercise", "Mental health", "Yoga", "Meditation", "Alternative medicine", 
+    "Nutrition", "Exercise", "Mental health", "Yoga", "Meditation", "Alternative medicine",
     "Wellness retreats", "Healthy living", "Stress management", "Self-care",
-    
+
     # Smartphone-related terms
-    "Smartphone", "Mobile device", "Operating system", "Android", "iOS", "Windows Phone", 
-    "UI (User Interface)", "Touchscreen", "Processor", "RAM (Random Access Memory)", 
-    "Internal storage", "Camera", "Megapixels", "Dual camera", "Battery capacity", 
-    "mAh (milliampere-hour)", "Charging technology", "Fast charging", "Wireless charging", 
-    "Display", "Screen size", "Resolution", "Pixel density", "Gorilla Glass", 
-    "Water resistance", "IP rating", "Fingerprint sensor", "Face recognition", 
-    "Biometric authentication", "NFC (Near Field Communication)", "Bluetooth", "Wi-Fi", 
-    "5G", "LTE", "SIM card", "Expandable storage", "USB Type-C", "Headphone jack", 
-    "Smartphone brand", "Apple", "Samsung", "Google", "Huawei", "Xiaomi", "OnePlus", 
-    "Sony", "LG", "Motorola", "Nokia", "HTC", "BlackBerry", "Lenovo", "Oppo", 
-    "Vivo", "Realme", "ASUS", "ZTE", "Alcatel", "Honor", "Meizu", "Infinix", 
-    "Tecno", "Itel", "Micromax", "Panasonic", "Sharp", "TCL", "LeEco", "Gionee", 
+    "Smartphone", "Mobile device", "Operating system", "Android", "iOS", "Windows Phone",
+    "UI (User Interface)", "Touchscreen", "Processor", "RAM (Random Access Memory)",
+    "Internal storage", "Camera", "Megapixels", "Dual camera", "Battery capacity",
+    "mAh (milliampere-hour)", "Charging technology", "Fast charging", "Wireless charging",
+    "Display", "Screen size", "Resolution", "Pixel density", "Gorilla Glass",
+    "Water resistance", "IP rating", "Fingerprint sensor", "Face recognition",
+    "Biometric authentication", "NFC (Near Field Communication)", "Bluetooth", "Wi-Fi",
+    "5G", "LTE", "SIM card", "Expandable storage", "USB Type-C", "Headphone jack",
+    "Smartphone brand", "Apple", "Samsung", "Google", "Huawei", "Xiaomi", "OnePlus",
+    "Sony", "LG", "Motorola", "Nokia", "HTC", "BlackBerry", "Lenovo", "Oppo",
+    "Vivo", "Realme", "ASUS", "ZTE", "Alcatel", "Honor", "Meizu", "Infinix",
+    "Tecno", "Itel", "Micromax", "Panasonic", "Sharp", "TCL", "LeEco", "Gionee",
     "Lava", "Nubia", "Redmi", "Poco", "Moto", "Vodafone", "Jio", "MTN", "Telstra"
 ]
 
 # classe per estrarre il testo dal pdf
+
+
 def extract_text_from_pdf(file_path):
     return extract_text(file_path)
 
